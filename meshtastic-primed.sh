b4b68fcfca0e9d417bc03b6795947bfbf4a8a6e4
andrewarabian <andrew.arabian.104@my.csun.edu>
2026-03-16 19:32:03 -0700

    Prepare workspace for Meshtastic CLI
    An automation script that configures Linux serial permissions for Meshtastic by ensuring the dialout group exists and adding the current user to it.

--- meshtastic-primed.sh
+++ meshtastic-primed.sh
@@ -2,6 +2,7 @@
 # / /\eshtastic mounting script for (Linux) - March 8, 2026 14:35 PM PST
 # https://github.com/andrewarabian
 # This simple script mounts your node without hassle, it streamlines the usergroup assignment process.
+#
 echo -e "\e[32mAssigning current user to the dialout group...\e[0m"
 sudo usermod -a -G dialout $USER
 echo -e "\e[32mMounting dialout...\e[0m"
